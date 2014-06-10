6458352723df6d374654cd038b9ff78f2677ddac
VladimirNik <vladimir.nikolaev9@gmail.com>
2014-06-11 01:52:09 +0400

    Quasiquotes fixed

--- src/compiler/scala/tools/nsc/typechecker/QuasiquotesImpl.scala
+++ src/compiler/scala/tools/nsc/typechecker/QuasiquotesImpl.scala
@@ -6,10 +6,8 @@ import scala.tools.reflect.quasiquotes.Quasiquotes
 //TODO-REFLECT - move to typechecker package and analyzer cake?
 trait QuasiquotesImpl {
   self: Global =>
-  override def context2quasiquoteImpl(c0: analyzer.MacroContext): Quasiquotes { val c: c0.type } = ???
-  //TODO-REFLECT uncomment this impl
-//  {
-//    val context = c0.asInstanceOf[scala.reflect.macros.contexts.Context]
-//    (new { val c: context.type = context } with Quasiquotes).asInstanceOf[Quasiquotes { val c: c0.type }]
-//  }
+  override def context2quasiquoteImpl(c0: analyzer.MacroContext): Quasiquotes { val c: c0.type } = {
+    val context = c0.asInstanceOf[scala.reflect.macros.contexts.Context]
+    (new { val c: context.type = context } with Quasiquotes).asInstanceOf[Quasiquotes { val c: c0.type }]
+  }
 }
