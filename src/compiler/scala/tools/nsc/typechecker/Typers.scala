a7afb8c58a73be4dd276194864ea39a36128d600
VladimirNik <vladimir.nikolaev9@gmail.com>
2014-07-19 17:19:13 +0400

    changes to newTyper which arise during quick.reflect compilation

--- src/compiler/scala/tools/nsc/typechecker/Typers.scala
+++ src/compiler/scala/tools/nsc/typechecker/Typers.scala
@@ -93,9 +93,10 @@ trait Typers extends Adaptations with Tags with TypersTracking with PatternTyper
 
   case class SilentResultValue[+T](value: T) extends SilentResult[T] { override def isEmpty = false }
 
-  def newTyper(context: Context): Typer = new NormalTyper(context)
+  def newTyper(context: Context): Typer = newTyper(context, false)
+  def newTyper(context: Context, useContextMirror: Boolean): Typer = new NormalTyper(context, useContextMirror)
 
-  private class NormalTyper(context : Context) extends Typer(context)
+  private class NormalTyper(context : Context, useContextMirror: Boolean) extends Typer(context, useContextMirror)
 
   // A transient flag to mark members of anonymous classes
   // that are turned private by typedBlock
@@ -104,7 +105,7 @@ trait Typers extends Adaptations with Tags with TypersTracking with PatternTyper
   private final val InterpolatorCodeRegex  = """\$\{.*?\}""".r
   private final val InterpolatorIdentRegex = """\$[$\w]+""".r // note that \w doesn't include $
 
-  abstract class Typer(context0: Context) extends TyperDiagnostics with Adaptation with Tag with PatternTyper with TyperContextErrors {
+  abstract class Typer(context0: Context, val useContextMirror: Boolean = false) extends TyperDiagnostics with Adaptation with Tag with PatternTyper with TyperContextErrors {
     import context0.unit
     import typeDebug.{ ptTree, ptBlock, ptLine, inGreen, inRed }
     import TyperErrorGen._
@@ -460,7 +461,7 @@ trait Typers extends Adaptations with Tags with TypersTracking with PatternTyper
     final def constrTyperIf(inConstr: Boolean): Typer =
       if (inConstr) {
         assert(context.undetparams.isEmpty, context.undetparams)
-        newTyper(context.makeConstructorContext)
+        newTyper(context.makeConstructorContext, false)
       } else this
 
     @inline
